062a948bb05c43721eebdadd094c9603c1702f8d
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2022-04-18 18:38:44 -0500

    chtc files

--- code/simulation/sim.sh
+++ code/simulation/sim.sh
@@ -5,7 +5,7 @@ wget https://figshare.com/ndownloader/files/24539828  -O  ../../data/raw_data/hu
 #wget https://figshare.com/ndownloader/files/25721792 -O ../../data/raw_data/small_atac_peaks.h5ad
 #wget https://figshare.com/ndownloader/files/25721789 -O ../../data/raw_data/small_atac_gene_activity.h5ad
 
-cd code/simulation
+cd scRNA_int/code/simulation
 Rscript -e "rmarkdown::render('simulate.Rmd', 
                   params = list(raw_data = '$raw_data',
                                 c_type = '$c_type',
@@ -13,7 +13,8 @@ Rscript -e "rmarkdown::render('simulate.Rmd',
                                 cell_times = '$cell_times',
                                 seq_depth = '$seq_depth'),
                   output_file = 'simulate_human_pancreas_'$c_type'_'$n_cell'_'$cell_times'_'$seq_depth'.html')"
-git add *
-git add ../../data/derived_data/*
-git commit -m "update '$c_type'_'$n_cell'_'$cell_times'_'$seq_depth'"
-git push
+
+cd ../../data
+rm -r raw_data/*
+tar -czf derived_'$c_type'_'$n_cell'_'$cell_times'_'$seq_depth'.tar.gz derived_data/
+mv *tar.gz ~
