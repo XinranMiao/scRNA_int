3e2e618bee2140b6db91a141080d97258a72679d
xmiao27 <xmiao27@submit2.chtc.wisc.edu>
2022-04-18 18:53:48 -0500

    debug

--- code/simulation/sim.sh
+++ code/simulation/sim.sh
@@ -4,7 +4,7 @@ git clone https://github.com/XinranMiao/scRNA_int
 wget https://figshare.com/ndownloader/files/24539828  -O  ../../data/raw_data/human_pancreas_norm_complexBatch.h5ad
 #wget https://figshare.com/ndownloader/files/25721792 -O ../../data/raw_data/small_atac_peaks.h5ad
 #wget https://figshare.com/ndownloader/files/25721789 -O ../../data/raw_data/small_atac_gene_activity.h5ad
-
+ls -l ../../data/raw_data/
 cd scRNA_int/code/simulation
 Rscript -e "rmarkdown::render('simulate.Rmd', 
                   params = list(raw_data = '$raw_data',
@@ -12,7 +12,7 @@ Rscript -e "rmarkdown::render('simulate.Rmd',
                                 n_cell = '$n_cell',
                                 cell_times = '$cell_times',
                                 seq_depth = '$seq_depth'),
-                  output_file = 'simulate_human_pancreas_'$c_type'_'$n_cell'_'$cell_times'_'$seq_depth'.html')"
+                  output_file = 'simulate_human_pancreas_"$c_type"_"$n_cell"_"$cell_times"_"$seq_depth".html')"
 
 cd ../../data
 rm -r raw_data/*
