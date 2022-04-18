ff75768656192b00aef0e381579f8d61a4b3236e
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2022-04-18 18:23:15 -0500

    update code

---
+++ code/simulation/generate_queue.R
@@ -0,0 +1,25 @@
+setwd("~/Documents/projects/s22_877/scRNA_int/code/simulation/")
+library(dplyr)
+q1 <- data.frame(
+  raw_data = "human_pancreas_norm_complexBatch.h5ad",
+  c_type = rep(c("acinar", "activated_stellate", "alpha", "beta",
+                 "delta", "ductal",
+                 "endothelial", "epsilon"), each =  9),
+  n_cell = "0",
+  cell_times = rep(c("0", "2", "5"), 24),
+  seq_depth = rep(rep(c(1000, 5000, 1e4), 3), each = 8)
+)
+
+
+q2 <- data.frame(
+  raw_data = "human_pancreas_norm_complexBatch.h5ad",
+  c_type = "multiple",
+  n_cell = rep(c("2", "2", "2", "5","5","5", "10", "10", "10"), 3),
+  cell_times = rep(c("0", "2", "5"), 9),
+  seq_depth = rep(rep(c(1000, 5000, 1e4), 3), each = 9)
+)
+
+
+
+rbind(q1, q2) %>%
+  write.table(file = "params.txt", quote = FALSE, row.names = FALSE, col.names = FALSE, sep = ",")
---
+++ code/simulation/sim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/XinranMiao/scRNA_int
+
+wget https://figshare.com/ndownloader/files/24539828  -O  ../../data/raw_data/human_pancreas_norm_complexBatch.h5ad
+#wget https://figshare.com/ndownloader/files/25721792 -O ../../data/raw_data/small_atac_peaks.h5ad
+#wget https://figshare.com/ndownloader/files/25721789 -O ../../data/raw_data/small_atac_gene_activity.h5ad
