45c4d29a49750df14451efd3b847387f584f14b1
xmiao27 <xmiao27@submit2.chtc.wisc.edu>
2022-04-19 13:42:26 -0500

    change requirement

--- code/simulation/sim.sh
+++ code/simulation/sim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "here" 1>&2
+ls -l . > return.txt
 git clone https://github.com/XinranMiao/scRNA_int
 mkdir scRNA_int/data/derived_data/human_pancreas_norm_complexBatch
 
@@ -8,21 +9,23 @@ cd scRNA_int/code/simulation
 #wget https://figshare.com/ndownloader/files/24539828  -O  ../../data/raw_data/human_pancreas_norm_complexBatch.h5ad
 #wget https://figshare.com/ndownloader/files/25721792 -O ../../data/raw_data/small_atac_peaks.h5ad
 #wget https://figshare.com/ndownloader/files/25721789 -O ../../data/raw_data/small_atac_gene_activity.h5ad
-ls -l . 1>&2
+
 
 Rscript -e "rmarkdown::render('simulate.Rmd', 
-                  params = list(raw_data = '$raw_data',
-                                c_type = '$c_type',
-                                n_cell = '$n_cell',
-                                cell_times = '$cell_times',
-                                seq_depth = '$seq_depth'),
-                  output_file = 'simulate_human_pancreas_"$c_type"_"$n_cell"_"$cell_times"_"$seq_depth".html')"
+                  params = list(raw_data = '$1',
+                                c_type = '$2',
+                                n_cell = '$3',
+                                cell_times = '$4',
+                                seq_depth = '$5'),
+                  output_file = 'simulate_human_pancreas_"$1"_"$2"_"$3"_"$4".html')"
 
 cd ../../data
 rm -r raw_data/*
-ls -l 1>&2
-tar -czf derived_$(c_type)_$(n_cell)_$(cell_times)_$(seq_depth).tar.gz derived_data
+ls -l derived_data/ 1>&2
+#tar -czf derived_$(c_type)_$(n_cell)_$(cell_times)_$(seq_depth).tar.gz derived_data
+tar -czf derived_$1_$2_$3_$4.tar.gz derived_data
+
 mv derived_data/*rds ../..
-mv derived_$(c_type)_$(n_cell)_$(cell_times)_$(seq_depth).tar.gz ../..
+mv derived_$1_$2_$3_$4.tar.gz ../..
 ls -l ../.. 1>&2
 cd ../..
