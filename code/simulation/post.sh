1d660ef643100222f052000a1504822bf0f65c1b
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2022-04-23 14:47:49 -0500

    clean results

--- code/simulation/post.sh
+++ code/simulation/post.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 scp xmiao27@submit2.chtc.wisc.edu:/home/xmiao27/integration/scRNA_int/code/simulation/*tar.gz ../../results/simulation/test/
+cd ../../results/simulation/test/
+ls  *tar.gz > list_tarball.txt
+files="list_tarball.txt"
+while IFS= read -r line
+do
+  tar -xzf "$line"
+done < "$files"
