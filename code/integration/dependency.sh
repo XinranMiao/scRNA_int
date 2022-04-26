3fb9543e94426b37212e509d0c770362b006081d
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2022-04-25 19:16:17 -0500

    debug param

--- code/integration/dependency.sh
+++ code/integration/dependency.sh
@@ -2,7 +2,10 @@
 export CC=/usr/local/Cellar/gcc/8.2.0/bin/gcc-8
 pip install mnnpy
 
-
+conda create -n scvi-env2 python=3.9
+conda activate scvi-env2
 conda install scvi-tools -c conda-forge
-
+pip install scib
+pip install scgen
+conda install -c conda-forge leidenalg
 pip install trVAE
