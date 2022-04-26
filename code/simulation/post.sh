dcfec619cf5750ed637b925690b28852bb659f72
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2022-04-26 12:11:57 -0500

    debug params

--- code/simulation/post.sh
+++ code/simulation/post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scp xmiao27@submit2.chtc.wisc.edu:/home/xmiao27/integration/scRNA_int/code/simulation/*tar.gz ../../results/simulation/test/
-cd ../../results/simulation/test/
+scp xmiao27@submit2.chtc.wisc.edu:/home/xmiao27/integration/scRNA_int/code/simulation/*tar.gz ../../results/simulation2/test/
+cd ../../results/simulation2/test/
 ls  *tar.gz > list_tarball.txt
 files="list_tarball.txt"
 while IFS= read -r line
