9f952d496dbeed2f30e99a8206dcdecbb3324ea8
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2022-04-29 10:50:24 -0500

    integrate using scanvi

--- code/integration/data_manage.py
+++ code/integration/data_manage.py
@@ -1,4 +1,9 @@
 import scanpy as sc
+import scvi
+import pymde
+from scvi.model.utils import mde
+
+
 def path2para(path, type = "sim_immune"):
     split_str = path.split("/")[-2].split("_")
     if type == "sim_immnue": 
@@ -34,3 +39,33 @@ def scgen_vis(path, adata, res, fig_path):
     sc.pp.neighbors(res, use_rep="corrected_latent")
     sc.tl.umap(res)
     sc.pl.umap(res, color=['tech', 'celltype'], wspace=0.4, frameon=False, save = fig_path + "_latent_umap.png")
+
+
+def scvi_vis(adata, vae, fig_path):
+    adata.obsm["X_scVI"] = vae.get_latent_representation()
+    sc.pp.neighbors(adata, use_rep="X_scVI")
+    sc.tl.leiden(adata)
+    adata.obsm["X_mde"] = mde(adata.obsm["X_scVI"])
+    sc.pl.embedding(
+        adata,
+        basis="X_mde",
+        color=["tech", "celltype"],
+        frameon=False,
+        ncols=1,
+        save = fig_path + "scvi_corrected_latent"
+    )
+
+
+def scanvi_vis(adata, vae, fig_path):
+    adata.obsm["X_scANVI"] = vae.get_latent_representation()
+    sc.pp.neighbors(adata, use_rep="X_scANVI")
+    sc.tl.leiden(adata)
+    adata.obsm["X_mde_scanvi"] = mde(adata.obsm["X_scANVI"])
+    sc.pl.embedding(
+        adata,
+        basis="X_mde_scanvi",
+        color=["tech", "celltype"],
+        frameon=False,
+        ncols=1,
+        save = fig_path + "scanvi_corrected_latent"
+    )
